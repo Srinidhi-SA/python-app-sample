0d2caa6a7ca886f847309ef8312dc10e294ded3a
Srinidhi-SA <srinidhi.anantharamu@marlabs.com>
2020-03-11 19:19:57 +0530

    Testing Changes

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World folks from mAdvisor!"
+    return "Hello World folks from mAdvisor team!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("8000"), debug=True)
